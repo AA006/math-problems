457a5706e94d75ef6e90459d247c1642fad98664
AA006 <73230787+AA006@users.noreply.github.com>
2024-07-21 02:16:30 +0300

    math problems

---
+++ math_projects1.py
@@ -0,0 +1,16 @@
+import math
+def us(x, y):
+    t = int(math.pow(x, y))
+    return t
+
+
+def kontrol(i):
+    a = us(i, 1) + us(i, 2) + us(i, 3) + us(i, 4) + us(i, 0)
+    for j in range(0, a):
+        if j == math.sqrt(a):
+            print(i, " ", a, " ", j)
+
+
+for i in range(0, 10):
+    print(i)
+    kontrol(i)
---
+++ math_projects2.py
@@ -0,0 +1,17 @@
+import math
+
+def us(x, y):
+    t = int(math.pow(x, y))
+    return t
+
+
+def kontrol(i):
+    z = int((us(i, 5)-1)/2)
+    for k in range(1, z):
+        if k == math.sqrt(z):
+            print(i, k, z)
+
+
+for i in range(1000):
+    print(i)
+    kontrol(i)
